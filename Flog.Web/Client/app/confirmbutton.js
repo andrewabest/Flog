54a19e929f915e2e5fc5e0b1e62b4ec8ec3eab34
Andrew Best <andrew.best@readify.net>
2014-12-30 11:39:53 +1000

    Removal of exercises with a funky little animated confirmation via a directive.

---
+++ Flog.Web/Client/app/confirmbutton.js
@@ -0,0 +1,41 @@
+﻿(function(angular) {
+    angular.module('app')
+        .directive("confirmButton", function () {
+            return {
+                priority: -100,  //<--------- Ensure that this executes before other directives such as ng-click
+                restrict: 'A',
+                link: function (scope, element, attrs) {
+
+                    var awaitingConfirmation = false;
+                    var originalText = '';
+                
+                    return element.bind('click', function (e) {
+
+                        if (!awaitingConfirmation) {
+
+                            e.stopImmediatePropagation();
+                            e.preventDefault();
+                            var $element = angular.element(element);
+                            awaitingConfirmation = true;
+                            originalText = $element.text();
+
+                            $element.text('Are you sure?');
+                            $element.removeClass('btn-danger');
+                            $element.addClass('btn-warning');
+
+                            window.setTimeout(function () {
+
+                                if (awaitingConfirmation) {
+
+                                    $element.text(originalText);
+                                    $element.removeClass('btn-warning');
+                                    $element.addClass('btn-danger');
+                                    awaitingConfirmation = false;
+                                }
+                            }, 3000);
+                        }
+                    });
+               }
+        };
+    });
+})(angular);
--- Flog.Web/Client/app/features/workout_edit/workout_details.js
+++ Flog.Web/Client/app/features/workout_edit/workout_details.js
@@ -10,13 +10,19 @@
         vm.exercises = [];
         vm.addExercise = addExercise;
         vm.addingExercise = false;
-
+        vm.removeExercise = removeExercise;
         // This needs a list of exercises that for each one I can then pass into the exercise directive. I could also put the "Add" method on here and add a new
         // exercise to this collection.
 
         function addExercise() {
 
             vm.addingExercise = true;
+        }
+
+        function removeExercise(exercise) {
+
+            var index = this.exercises.indexOf(exercise);
+            this.exercises.splice(index, 1);
         }
     }
 
