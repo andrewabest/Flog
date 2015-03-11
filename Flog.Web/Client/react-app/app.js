1e8ad55c856ee50485b4ed8c9baf1e0cc1a3df2b
Andrew Best <andrew.best@readify.net>
2015-03-11 12:04:12 +1000

    Pulled JS dependencies locally. Added routing for workout pages.

--- Flog.Web/Client/react-app/app.js
+++ Flog.Web/Client/react-app/app.js
@@ -1,23 +1,60 @@
 ﻿(function() {
 	'use strict';
 
-	var Workout = React.createClass({
+	var Router = ReactRouter;
+	var DefaultRoute = Router.DefaultRoute;
+	var Link = Router.Link;
+	var Route = Router.Route;
+	var NotFoundRoute = Router.NotFoundRoute;
+	var Redirect = Router.Redirect;
+	var RouteHandler = Router.RouteHandler;
+	var Navigation = Router.Navigation;
 
-		editWorkout: function() {
-			alert('edit!');
-		},
+	var NewWorkout = React.createClass({
 
 		render: function () {
 			return (
-				<div><a onClick={this.editWorkout}>{this.props.workout.display}</a></div>
+				<h1>Hello New Workout!</h1>
 			);
+		}
+	});
+
+	var Workout = React.createClass({
+
+		render: function () {
+			return (
+				<h1>Hello Workout!</h1>
+			);
+		}
+	});
+
+	var WorkoutListItem = React.createClass({
+
+		propTypes: {
+			workout: React.PropTypes.object.isRequired,
+		},
+
+		render: function () {
+			return <li><Link to="editWorkout" params={{id: this.props.workout.id}}>{this.props.workout.display}</Link></li>;
 		}
 	});
 	
 	var WorkoutList = React.createClass({
 
+		propTypes: {
+			workouts: React.PropTypes.array,
+		},
+
+		getDefaultProps: function() {
+		    return {
+		    	workouts: [{key:0, id:0, display: 'Workout one'}, {key:1, id:1, display: 'Workout two'}]
+		    };
+	  	},
+
+	  	mixins: [Navigation],
+
 		beginWorkout: function() {
-			alert('begin!');
+			this.transitionTo('newWorkout');
 		},
 
 		render: function() {
@@ -44,9 +81,11 @@
 				    </div>
 				    <div className="row" style={workoutRowStyle}>
 				        <div className="col-md-12 well">
+				        	<ul>
 					        {this.props.workouts.map(function(workout, i) {
-					            return <Workout workout={workout} />;
+					            return <WorkoutListItem workout={workout} />;
 					        })}
+					        </ul>
 				        </div>
 				    </div>
 			    </div>
@@ -54,19 +93,41 @@
 		}
 	});
 
+	
+
+	var NotFound = React.createClass({
+		render: function() {
+			return  <p>Do or do not.<br />There is no route.<br />-Yoda</p>;
+		}
+	});
+
+	var WorkoutNotFound = React.createClass({
+		render: function() {
+			return  <p>Do or do not.<br />There is no workout.<br />-Yoda</p>;
+		}
+	});
+
 	var App = React.createClass({
 	    render: function(){
 	        return (
-	            <WorkoutList workouts={[{display: 'Workout one'}, {display: 'Workout two'}]} />
+	        	<RouteHandler />
 	        );
 	    }
 	});
 
-	window.loading_screen.finish();
+	var routes = (
+		<Route path="/" handler={App}>
+			<Route name="workoutList" path="react" handler={WorkoutList} />
+			<Route name="newWorkout" path="react/new" handler={NewWorkout} />
+			<Route name="editWorkout" path="react/:id" handler={Workout} />
+			<NotFoundRoute handler={NotFound}/>
+	  	</Route>
+	);
 
-	React.render(
-        <App />,
-        document.getElementById('app')
-    );
+	Router.run(routes, Router.HistoryLocation, function (Handler) {
+	  React.render(<Handler/>, document.getElementById('app'));
+	});
+
+	window.loading_screen.finish();
 
 })();
